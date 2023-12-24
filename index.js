158a2bb8f1ba8a2e448ec10f4e84391695f76cae
KAMAL <kamaltejp@gmail.com>
2023-12-24 14:36:35 +0530

    last commit

--- index.js
+++ index.js
@@ -35,10 +35,7 @@ app.use("/changePassword", ChangePasswordRoute);
 
 
 mongoose
-  .connect(process.env.MONGODB_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("mongodb is connected");
   })
